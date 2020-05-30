8c60d4bbb02ab73b2345a3ef1b43aad4c8ed4680
Minjeong Kim <lona573@gmail.com>
2020-05-30 10:34:32 +0900

    검색 버튼 추가

--- CDP_Canworks/app/src/main/java/com/example/cdp_canworks/Main.java
+++ CDP_Canworks/app/src/main/java/com/example/cdp_canworks/Main.java
@@ -2,10 +2,14 @@ package com.example.cdp_canworks;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ImageButton;
 import android.widget.ListView;
+import android.widget.SearchView;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -16,6 +20,7 @@ import java.util.ArrayList;
 public class Main extends AppCompatActivity implements AdapterView.OnItemClickListener {
 
     private long time;
+    ListView listView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,9 +31,10 @@ public class Main extends AppCompatActivity implements AdapterView.OnItemClickLi
         setSupportActionBar(tb);
         getSupportActionBar().setDisplayShowTitleEnabled(false);
 
-        ListView listView;
         ListViewAdapter adapter;
         ArrayList<ListViewItem> items = new ArrayList<ListViewItem>();
+        ArrayList<ListViewItem> copyItems = new ArrayList<ListViewItem>();
+        copyItems.addAll(items);
 
         adapter = new ListViewAdapter(this) ;
         listView = (ListView)findViewById(R.id.listview1);
@@ -38,20 +44,8 @@ public class Main extends AppCompatActivity implements AdapterView.OnItemClickLi
         adapter.addItem("캔웍스", "012345");
         adapter.addItem("카페 봉봉", "003235");
         adapter.addItem("커피왕", "082745");
-        
-        ImageButton logout = (ImageButton)findViewById(R.id.logout);
+
         ImageButton create = (ImageButton)findViewById(R.id.create);
-
-        logout.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(Main.this, Login.class);
-                Toast.makeText(getApplicationContext(), "로그아웃 되었습니다.", Toast.LENGTH_SHORT).show();
-                startActivity(intent);
-                finish();
-            }
-        });
-
         create.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -60,8 +54,45 @@ public class Main extends AppCompatActivity implements AdapterView.OnItemClickLi
             }
         });
 
-
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.toolbar_action, menu);
+        SearchView searchView = (SearchView)menu.findItem(R.id.action_search).getActionView();
+        searchView.setMaxWidth(Integer.MAX_VALUE);
+        searchView.setQueryHint("가게명으로 검색합니다.");
+        searchView.setOnQueryTextListener(queryTextListener);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        Intent intent;
+        switch (item.getItemId()) {
+            case R.id.real_logout:
+                intent = new Intent(Main.this, Login.class);
+                Toast.makeText(getApplicationContext(), "로그아웃 되었습니다.", Toast.LENGTH_SHORT).show();
+                startActivity(intent);
+                finish();
+        }
+        return false;
+    }
+
+    private SearchView.OnQueryTextListener queryTextListener = new SearchView.OnQueryTextListener() {
+        @Override
+        public boolean onQueryTextSubmit(String query) {
+            return false;
+        }
+
+        @Override
+        public boolean onQueryTextChange(String s) {
+
+            return false;
+        }
+    };
+
 
     @Override
     public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
